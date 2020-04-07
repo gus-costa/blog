ceb3ed4a44827f5ba62ab332996886f84a50f762
Gus Costa <gustavo@gustavoalves.com.br>
2020-04-07 18:00:10 -0300

    adding linkedin button to footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -63,6 +63,14 @@
         <div class="col-lg-8 col-md-10 mx-auto">
           <ul class="list-inline text-center">
             <li class="list-inline-item">
+              <a href="https://www.linkedin.com/in/gus-costa/" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-linkedin fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
               <a href="https://twitter.com/gstvac" target="_blank">
                 <span class="fa-stack fa-lg">
                   <i class="fas fa-circle fa-stack-2x"></i>
